edf4180862a4e49e118c116be8e56c044df87270
Olli Vanhoja <olli.vanhoja@gmail.com>
2015-11-06 23:22:15 +0200

    Add uround_up()

--- include/xstack_util.h
+++ include/xstack_util.h
@@ -57,4 +57,9 @@ static inline unsigned long ulmax(unsigned long a, unsigned long b)
 static inline unsigned long ulmin(unsigned long a, unsigned long b)
 { return (a < b ? a : b); }
 
+static inline unsigned int uround_up(unsigned n, unsigned s)
+{
+    return ((n + s - 1) / s) * s;
+}
+
 #endif /* XSTACK_UTIL */
